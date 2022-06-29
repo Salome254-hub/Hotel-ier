a7e2131b3ff102676835c27914aa16a28ed8d5b1
Salome mabonga <salomenanjala254@gmail.com>
2022-06-29 15:28:31 +0300

    Edited my event listner

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const nav1 = (document.getElementsByTagName("li")[0]);
 const nav2 = (document.getElementsByTagName("li")[1]);
 const nav3 = (document.getElementsByTagName("li")[2]);
 nav1.addEventListener("click", function(){
-    console.log("Peru,Arizona,Italy, Hawaii,Kenya,New Zealand,Istanbul, Turkey, Cambodia") 
+    console.log("Popular places was clicked") 
 });
 
 nav2.addEventListener('click', function(){
-    console.log(" Asia, Africa,North America,South America,Antarctica,Europe,Australia")
+    console.log("Travel outside was clicked")
 });
 
 nav2.addEventListener('click', function(){
-    console.log(" Parking,Free Wifi,Room Service,24-Hour Guest Reception,Complimentary Toiletries,Healthy Breakfast,Ample Wall Outlets,Hair Styling Tools,Flexible Checkout,PoolMini-fridge,Complimentary Electronics Chargers,Business Facilities,Transportation Information,Laundry Services,Spa & Wellness Amenities,Exercise Facilities and Accessories,Daily Newspaper,Entertainment,Complimentary Luggage storage,Cribs & Cots for Children,Custom Offers,Curated Experiences,Kid-friendly Rooms and Products,Premium Bedding,Pet-friendly Rooms,Champagne Bar")
+    console.log("Services was clicked")
 });
