0f7153ba02d0e130b3d1e54304e0e3f56653cf0b
Salome mabonga <salomenanjala254@gmail.com>
2022-06-29 15:09:13 +0300

    Added click event listners to my navigation list

---
+++ index.js
@@ -0,0 +1,14 @@
+const nav1 = (document.getElementsByTagName("li")[0]);
+const nav2 = (document.getElementsByTagName("li")[1]);
+const nav3 = (document.getElementsByTagName("li")[2]);
+nav1.addEventListener("click", function(){
+    console.log("Peru,Arizona,Italy, Hawaii,Kenya,New Zealand,Istanbul, Turkey, Cambodia") 
+});
+
+nav2.addEventListener('click', function(){
+    console.log(" Asia, Africa,North America,South America,Antarctica,Europe,Australia")
+});
+
+nav2.addEventListener('click', function(){
+    console.log(" Parking,Free Wifi,Room Service,24-Hour Guest Reception,Complimentary Toiletries,Healthy Breakfast,Ample Wall Outlets,Hair Styling Tools,Flexible Checkout,PoolMini-fridge,Complimentary Electronics Chargers,Business Facilities,Transportation Information,Laundry Services,Spa & Wellness Amenities,Exercise Facilities and Accessories,Daily Newspaper,Entertainment,Complimentary Luggage storage,Cribs & Cots for Children,Custom Offers,Curated Experiences,Kid-friendly Rooms and Products,Premium Bedding,Pet-friendly Rooms,Champagne Bar")
+});
